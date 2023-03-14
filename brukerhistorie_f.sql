deda0a44fd62b004fdc27a4520ae24dd16df3c0e
Magnus Brekke Nilsen <magnbni@stud.ntnu.no>
2023-03-14 14:02:51 +0100

    finsih a and b

--- brukerhistorie_f.sql
+++ brukerhistorie_f.sql
@@ -1,3 +1,6 @@
 /* Det skal legges inn nødvendige data slik at systemet kan håndtere billettkjøp for de tre togrutene
 på Nordlandsbanen, mandag 3. april og tirsdag 4. april i år. Dette kan gjøres med et skript, dere
 trenger ikke å programmere støtte for denne funksjonaliteten */
+
+alter table Togrutetabell
+add avgangstid time;

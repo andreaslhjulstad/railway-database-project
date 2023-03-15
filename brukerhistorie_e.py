cd2f982d007eb0f03eddbab3cee7affd7e46c725
Andreas Lilleby Hjulstad <andrelhj@stud.ntnu.no>
2023-03-15 11:00:30 +0100

    Finish user story e

--- brukerhistorie_e.py
+++ brukerhistorie_e.py
@@ -1,1 +1,78 @@
 """ En bruker skal kunne registrere seg i kunderegisteret. Denne funksjonaliteten skal programmeres. """
+
+import sqlite3
+import re
+
+
+# Opprett tilkobling til databasen
+con = sqlite3.connect("jernbaneDatabase.db")
+cursor = con.cursor()
+
+
+# Skript for å spørre brukeren om nødvendig informasjon
+def get_user_info():
+    name = input("Skriv inn fornavnet ditt: ").lower()
+    # Sjekker at navnet består av bare bokstaver og er 2-30 tegn, og gir brukeren et nytt forsøk dersom det ikke er det
+    while not re.fullmatch('[a-z]{2,30}', name):
+        print("Ugyldig navn! Navnet må bestå av bokstaver og være 2-30 tegn langt")
+        name = input("Skriv inn fornavnet ditt: ").lower()
+    # Gjør forbokstaven i navnet stor
+    name = name[0].upper() + name[1:]
+
+    email = input("Skriv inn e-postadressen din: ")
+    # Sjekker at e-posten er på gyldig format, og gir brukeren et nytt forsøk dersom det ikke er det
+    while not re.fullmatch(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+', email):
+        print("Ugyldig e-postadresse!")
+        email = input("Skriv inn e-postadressen din: ").lower()
+
+    phoneno = input("Skriv inn mobilnummeret ditt (uten landskode): ")
+    # Sjekker at nummeret er et gyldig norsk telefonnummer, og gir brukeren et nytt forsøk dersom det ikke er det
+    while not re.fullmatch('[49]{1}[0-9]{7}', phoneno):
+        print("Ugyldig mobilnummer! Nummeret må være et gyldig norsk telefonnummer")
+        phoneno = input(
+            "Skriv inn mobilnummeret ditt (uten landskode): ").lower()
+    return (name, email, phoneno)
+
+
+# Henter neste kundenr ved å sjekke kundenr som sist ble lagret i databasen
+def get_next_customerno():
+    cursor.execute("SELECT kundenr FROM Kunde")
+    rows = cursor.fetchall()
+
+    max_customerno = 0
+    for row in rows:
+        if row[0] > max_customerno:
+            max_customerno = row[0]
+    return max_customerno + 1
+
+
+# Kjører SQL-skript for å sette inn verdiene i databasen
+def insert_values(customerno, name, email, phoneno):
+    insert = """INSERT INTO Kunde (kundenr, navn, epost, telefonnr) 
+            VALUES (?, ?, ?, ?)"""
+    data_tuple = (customerno, name, email, phoneno)
+    cursor.execute(insert, data_tuple)
+
+    con.commit()
+
+
+def main():
+    print("----------------------------Registrer ny bruker----------------------------")
+    print("\n")
+
+    (name, email, phoneno) = get_user_info()
+    new_customerno = get_next_customerno()
+
+    insert_values(new_customerno, name, email, phoneno)
+
+    print("\n")
+    print("Brukeren ble registrert")
+    print("---------------------------------------------------------------------------")
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Lukker tilkobling til databasen
+con.close()

93e7cb712568ed1876b6b2f44ecde5a3280c7e9b
Andreas Lilleby Hjulstad <andrelhj@stud.ntnu.no>
2023-03-20 11:01:06 +0100

    Add check for if email is already registered

--- brukerhistorie_e.py
+++ brukerhistorie_e.py
@@ -24,6 +24,10 @@ def get_user_info():
     while not re.fullmatch(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+', email):
         print("Ugyldig e-postadresse!")
         email = input("Skriv inn e-postadressen din: ").lower()
+    
+    while email in get_registered_emails():
+        print("Den e-postadressen er allerede registrert!")
+        email = input("Skriv inn e-postadressen din: ").lower()
 
     phoneno = input("Skriv inn mobilnummeret ditt (uten landskode): ")
     # Sjekker at nummeret er et gyldig norsk telefonnummer, og gir brukeren et nytt forsøk dersom det ikke er det
@@ -44,6 +48,15 @@ def get_next_customerno():
         if row[0] > max_customerno:
             max_customerno = row[0]
     return max_customerno + 1
+
+def get_registered_emails():
+    cursor.execute("SELECT epost FROM Kunde")
+    rows = cursor.fetchall()
+    
+    registered_emails = []
+    for row in rows:
+        registered_emails.append(row[0])
+    return registered_emails
 
 
 # Kjører SQL-skript for å sette inn verdiene i databasen
